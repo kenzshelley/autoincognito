daeba8935a69fa6bbeb001be68d5f8bb3f068721
kenzshelley <kenzshelley@gmail.com>
2016-07-21 16:43:16 -0400

    Made activating different domains actually work

--- background.js
+++ background.js
@@ -3,13 +3,12 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (changeInfo.url) {
 
     chrome.storage.sync.get('domains', function(obj) {
-      for (var i in obj['domains']) {
-        var entry = obj['domains'][i];
-        if (entry.active) {
-          if (changeInfo.url.search(entry.domain) != -1) {
+      for (var domain in obj['domains']) {
+        var active = obj['domains'][domain];
+        if (active) {
+          if (changeInfo.url.search(domain) != -1) {
             var createData = {url: changeInfo.url, incognito: true};
-            chrome.windows.create(createData, function() {
-            });
+            chrome.windows.create(createData, function() {});
           }
         }
       }
--- options.js
+++ options.js
@@ -1,16 +1,41 @@
 $(document).ready(function() {
+  function toggleActive() {
+    var cb = $(this)[0];
+    if (cb.parentNode == undefined) return;
+    var domain = cb.parentNode.previousElementSibling.innerHTML;
+
+    chrome.storage.sync.get('domains', function(obj) {
+      var domains = obj['domains'];
+      if (domains == undefined) return;
+
+      if (cb.checked) {
+        domains[domain] = true;
+      } else {
+        domains[domain] = false;
+      }
+
+      chrome.storage.sync.set({'domains': domains}, function() {});
+    });
+  }
+
+  $(document).on('click', '.active', $(this),  toggleActive);
+
   function initializeTable() {
     chrome.storage.sync.get('domains', function(obj) {
-      for (var i in obj['domains']) {
-        var entry = obj['domains'][i];
+      for (var domain in obj['domains']) {
         var row = document.getElementById("urls_table").insertRow();
-        var domain = entry.domain;
-        var active = entry.active;
+        var active = obj['domains'][domain];
         row.insertCell(0).innerHTML = domain;
         if (active) {
-          row.insertCell(1).innerHTML = '<input type="checkbox" checked>';
+          var newCell = row.insertCell(1)
+          newCell.innerHTML = '<input class="active" type="checkbox" checked>';
+          var inputs = newCell.getElementsByTagName('input');
+          var input = inputs[0];
         } else {
-          row.insertCell(1).innerHTML = '<input type="checkbox">';
+          var newCell = row.insertCell(1);
+          newCell.innerHTML = '<input class="active" type="checkbox">';
+          var inputs = newCell.getElementsByTagName('input');
+          var input = inputs[0];
         }
       }    
     }); 
@@ -18,26 +43,28 @@ $(document).ready(function() {
 
   initializeTable();
 
-  var input = document.getElementById('new_url');
-  input.onkeypress = function(e) {
+  var domainInput = document.getElementById('new_url');
+  domainInput.onkeypress = function(e) {
     var event = e || window.event;
     var charCode = event.which || event.keyCode;
 
     if ( charCode == '13' ) {
       // Enter pressed
       var row = document.getElementById("urls_table").insertRow();
-      var domain = input.value;
-      input.value = '';
+      var domain = domainInput.value;
+      domainInput.value = '';
       row.insertCell(0).innerHTML = domain;
-      row.insertCell(1).innerHTML = '<input type="checkbox" checked>';
+      var newCell = row.insertCell(1)
+      newCell.innerHTML = '<input class="active" type="checkbox" checked>';
+      var inputs = newCell.getElementsByTagName('input');
+      var input = inputs[0];
 
       chrome.storage.sync.get('domains', function(obj) {
         var domains = obj['domains'];
         if (domains == undefined) {
-          domains = [{'domain':domain, 'active': true}]; 
-        } else {
-          domains.push({'domain':domain, 'active': true});
-        }
+          domains = {};
+        } 
+        domains[domain] = true;
 
         chrome.storage.sync.set({'domains': domains}, function() {});
       });
