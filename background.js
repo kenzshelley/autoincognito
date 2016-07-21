9c42efb4274cd22e9f20591aff780e48f87e1999
kenzshelley <kenzshelley@gmail.com>
2016-07-21 14:47:05 -0400

    Actually use options page & store settings using chrome sync storage.

--- background.js
+++ background.js
@@ -2,13 +2,18 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (changeInfo.url) {
 
-    var pat = "nytimes";
-    if (changeInfo.url.search(pat) != -1) {
-      var createData = {url: changeInfo.url, incognito: true};
-      chrome.windows.create(createData, function() {
-        console.log("Created new window!");
-      });
-    }
+    chrome.storage.sync.get('domains', function(obj) {
+      for (var i in obj['domains']) {
+        var entry = obj['domains'][i];
+        if (entry.active) {
+          if (changeInfo.url.search(entry.domain) != -1) {
+            var createData = {url: changeInfo.url, incognito: true};
+            chrome.windows.create(createData, function() {
+            });
+          }
+        }
+      }
+    });
   }
 });
 
--- options.js
+++ options.js
@@ -1,4 +1,23 @@
 $(document).ready(function() {
+  function initializeTable() {
+    chrome.storage.sync.get('domains', function(obj) {
+      for (var i in obj['domains']) {
+        var entry = obj['domains'][i];
+        var row = document.getElementById("urls_table").insertRow();
+        var domain = entry.domain;
+        var active = entry.active;
+        row.insertCell(0).innerHTML = domain;
+        if (active) {
+          row.insertCell(1).innerHTML = '<input type="checkbox" checked>';
+        } else {
+          row.insertCell(1).innerHTML = '<input type="checkbox">';
+        }
+      }    
+    }); 
+  }
+
+  initializeTable();
+
   var input = document.getElementById('new_url');
   input.onkeypress = function(e) {
     var event = e || window.event;
@@ -6,11 +25,22 @@ $(document).ready(function() {
 
     if ( charCode == '13' ) {
       // Enter pressed
-      console.log('Pressed enter!');
       var row = document.getElementById("urls_table").insertRow();
-      row.insertCell(0).innerHTML = input.value;
-      row.insertCell(1).innerHTML = '<input type="checkbox" checked>';
+      var domain = input.value;
       input.value = '';
+      row.insertCell(0).innerHTML = domain;
+      row.insertCell(1).innerHTML = '<input type="checkbox" checked>';
+
+      chrome.storage.sync.get('domains', function(obj) {
+        var domains = obj['domains'];
+        if (domains == undefined) {
+          domains = [{'domain':domain, 'active': true}]; 
+        } else {
+          domains.push({'domain':domain, 'active': true});
+        }
+
+        chrome.storage.sync.set({'domains': domains}, function() {});
+      });
 
       return false;
     }
